c3320634132756bf48074f4b8663fee92af6a037
Solodkov Alexander <solodkov.alexander.ru@gmail.com>
2024-08-28 13:14:42 +0300

    Обновлены pdf с содержимым.
    Реализованы некоторые вещи в классах.

--- design/cell.h
+++ design/cell.h
@@ -2,22 +2,223 @@
 
 #include "common.h"
 #include "formula.h"
+#include "sheet.h"
+
+#include <algorithm>
+#include <optional>
+#include <unordered_set>
+
+using namespace std::literals;
 
 class Cell : public CellInterface {
 public:
-    Cell();
-    ~Cell();
+    explicit Cell(Sheet& sheet) :
+        sheet_(sheet) 
+    {}
+    ~Cell() {}
 
-    void Set(std::string text);
-    void Clear();
+    void Set(std::string text) {
+        // Если происходит установка такого же текста: выход
+        if (impl_ && impl_->GetInitialText() == text) {
+            return;
+        }
 
-    Value GetValue() const override;
-    std::string GetText() const override;
+        // Признак, что текст ячейки - формула
+        bool is_formula = (!text.empty() && text.at(0) == '=' && text.size() > 1);
+        std::unique_ptr<FormulaInterface> formula;
+        if (is_formula) {
+            // Парсим формулу
+            formula = ParseFormula(std::string(text.begin() + 1, text.end()));
+            // Выполняем проверку на наличие циклической зависимости в новой формуле
+            if (CheckCircularDependency(formula->GetReferencedCells())) {
+                throw CircularDependencyException("Invalid formula: found circular dependency"s);
+            }
+        }
+
+        // Сбрасываем кэш (рекурсивно)
+        ClearValueCache();
+
+        // У ячеек, от которых зависело значение тек. ячейки: убираем связь
+        for (auto referenced_cell : GetReferencedCells()) {
+            sheet_.GetConcreteCell(referenced_cell)->cells_from_.erase(this);
+        }
+
+        // Определяем новую реализацию
+        if (is_formula) {
+            impl_ = std::make_unique<FormulaImpl>(text, std::move(formula), sheet_);
+
+            // У ячеек, от которых зависит значение тек. ячейки: устанавливаем связь к тек. ячейке
+            for (auto referenced_cell : GetReferencedCells()) {
+                if (!referenced_cell.IsValid()) {
+                    continue;
+                }
+                // Если ячейки из формулы не существует: создаем пустую ячейку
+                auto cell = sheet_.GetConcreteCell(referenced_cell);
+                if (!cell) {
+                    cell = sheet_.CreateEmptyCell(referenced_cell);
+                }
+                cell->cells_from_.insert(this);
+            }
+        } else {
+            impl_ = std::make_unique<TextImpl>(std::move(text));
+        }
+    }
+
+    void Clear() {
+        // Сбрасываем кэш (рекурсивно)
+        ClearValueCache();
+
+        impl_ = std::make_unique<EmptyImpl>();
+    }
+
+    Value GetValue() const override {
+        if (!value_cache_.has_value()) {
+            value_cache_ = impl_->GetValue();
+        }
+        return *value_cache_;
+    }
+
+    std::string GetText() const override {
+        return impl_->GetText();
+    }
+    
+    std::vector<Position> GetReferencedCells() const override {
+        if (!impl_) {
+            return {};
+        }
+        auto formula_impl = dynamic_cast<FormulaImpl*>(impl_.get());
+        if (!formula_impl) {
+            return {};
+        }
+        return formula_impl->GetReferencedCells();
+    }
+
+    bool IsEmpty() const {
+        return !impl_ || dynamic_cast<EmptyImpl*>(impl_.get());
+    }
 
 private:
-    class Impl;
-    class EmptyImpl;
-    class TextImpl;
-    class FormulaImpl;
+    class Impl {
+        public:
+            Impl() = default;
+            virtual ~Impl() = default;
+
+            virtual Value GetValue() const = 0;
+            virtual std::string GetText() const = 0;
+            virtual std::string GetInitialText() const = 0;
+    };
+    class EmptyImpl final : public Impl {
+        public:
+            Value GetValue() const override { return ""s; }
+            std::string GetText() const override { return ""s; }
+            std::string GetInitialText() const override { return ""s; }
+    };
+    class TextImpl final : public Impl {
+        public:
+            TextImpl(std::string text) :
+                text_(text) 
+            {}
+            Value GetValue() const override { 
+                return (text_.size() > 0 && text_.at(0) == '\''
+                        ? std::string(text_.begin() + 1, text_.end())
+                        : text_);
+            }
+            std::string GetText() const override { return text_; }
+            std::string GetInitialText() const override { return text_; }
+        private: 
+            std::string text_;
+    };
+    class FormulaImpl final : public Impl {
+        public:
+            FormulaImpl(std::string text, std::unique_ptr<FormulaInterface> formula, Sheet& sheet) :
+                text_(text),
+                formula_(std::move(formula)),
+                sheet_(sheet) {
+            }
+            Value GetValue() const override {
+                FormulaInterface::Value value = formula_->Evaluate(sheet_);
+                if (std::holds_alternative<double>(value)) {
+                    return std::get<double>(value);
+                }
+                return std::get<FormulaError>(value);
+            }
+            std::string GetText() const override {
+                return '=' + formula_->GetExpression(); 
+            }
+            std::string GetInitialText() const override { 
+                return text_; 
+            }
+            std::vector<Position> GetReferencedCells() const {
+                return formula_->GetReferencedCells();
+            }
+
+        private: 
+            std::string text_;
+            std::unique_ptr<FormulaInterface> formula_;
+            // таблица ячейки 
+            // (необходима для получения доступа к ячейкам в случае формульных ячеек, содержащих в формулах индексы на ячейки)
+            Sheet& sheet_;
+    };
+
+private:
+    void ClearValueCache() {
+        value_cache_ = std::nullopt;
+
+        // Если были ячейки, которые зависят от текущей ячейки: сбрасываем их кэш значений тоже
+        for (auto cell_from : cells_from_) {
+            cell_from->ClearValueCache();
+        }
+    }
+
+    bool CheckCircularDependency(const std::vector<Position>& referenced_cells) const {
+        for (auto referenced_cell : referenced_cells) {
+            std::vector<Position> checked_cells;
+            if (CheckCircularDependency(referenced_cell, checked_cells)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool CheckCircularDependency(Position cell_position, std::vector<Position>& checked_cells) const {
+        if (!cell_position.IsValid()) {
+            return false;
+        }
+        auto cell = sheet_.GetConcreteCell(cell_position);
+        if (!cell) {
+            return false;
+        }
+        if (cell == this) {
+            return true;
+        }
+    
+        // Помечаем ячейку, что мы её прошли
+        checked_cells.push_back(cell_position);
+
+        // Проходим по ячейкам, от которых зависит cell_position
+        auto next_cells = cell->GetReferencedCells();
+        for (auto next_cell : next_cells) {
+            if (std::find(checked_cells.begin(), checked_cells.end(), next_cell) != checked_cells.end()) {
+                return true;
+            }
+
+            // Продолжаем обход ячеек
+            if (CheckCircularDependency(next_cell, checked_cells)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+private:
     std::unique_ptr<Impl> impl_;
+    // таблица ячейки
+    Sheet& sheet_;
+
+    // ячейки, которые ссылаются на текущую ячейку (т.е. ячейки, чье вычисление значения зависит от текущей ячейки)
+    // (необходим для инвалидации кэша)
+    std::unordered_set<Cell*> cells_from_; 
+
+    // Кэш вычисленного значения
+    mutable std::optional<Value> value_cache_;
 };
--- design/sheet.h
+++ design/sheet.h
@@ -4,31 +4,161 @@
 #include "common.h"
 
 #include <functional>
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <variant>
+
+struct PositionHasher {
+    static const uint64_t N = 37;
+    size_t operator() (Position pos) const {
+        return static_cast<size_t>(pos.row * N + pos.col);
+    }
+};
 
 class Sheet : public SheetInterface {
 public:
-    ~Sheet();
+    ~Sheet() {}
 
-    void SetCell(Position pos, std::string text) override;
+    void SetCell(Position pos, std::string text) override {
+        // Если ячейки не существует
+        if (!HasConcreteCell(pos)) {
+            // Создаем ячейку
+            cells_[pos] = std::make_unique<Cell>(*this);
 
-    const CellInterface* GetCell(Position pos) const override;
-    CellInterface* GetCell(Position pos) override;
+            // Обновляем данные для вычисления размера печатной области
+            ++row_to_cell_count_[pos.row];
+            ++column_to_cell_count_[pos.col];
+        }
 
-    void ClearCell(Position pos) override;
+        // Устанавливаем содержимое ячейки
+        cells_[pos]->Set(text);
+    }
 
-    Size GetPrintableSize() const override;
+    Cell* CreateEmptyCell(Position pos) {
+        auto cell = GetConcreteCell(pos);
+        if (!cell) {
+            cells_[pos] = std::make_unique<Cell>(*this);
+            cells_[pos]->Clear();
+            cell = cells_[pos].get();
+        }
+        return cell;
+    }
 
-    void PrintValues(std::ostream& output) const override;
-    void PrintTexts(std::ostream& output) const override;
+    // Методы получения ячейки (для которых был вызван SetCell)
+    const CellInterface* GetCell(Position pos) const override {
+        if (!HasCell(pos)) {
+            return nullptr;
+        }
+        return cells_.at(pos).get();
+    }
+    CellInterface* GetCell(Position pos) override {
+        if (!HasCell(pos)) {
+            return nullptr;
+        }
+        return cells_[pos].get();
+    }
 
-    const Cell* GetConcreteCell(Position pos) const;
-    Cell* GetConcreteCell(Position pos);
+    void ClearCell(Position pos) override {
+        // Проверяем наличие ячейки (для которой был вызван SetCell)
+        if (!HasCell(pos)) {
+            return;
+        }
+        
+        // Обновляем данные для вычисления размера печатной области
+        --row_to_cell_count_[pos.row];
+        --column_to_cell_count_[pos.col];
+        if (row_to_cell_count_.at(pos.row) == 0) {
+            row_to_cell_count_.erase(pos.row);
+        }
+        if (column_to_cell_count_.at(pos.col) == 0) {
+            column_to_cell_count_.erase(pos.col);
+        }
+
+        // Превращаем ячейку в пустую ячейку
+        cells_[pos]->Clear();
+    }
+
+    Size GetPrintableSize() const override {
+        if (row_to_cell_count_.empty()) {
+            return {};
+        }
+
+        // За счет сортировки в map-ах: 
+        // последний элемент в row_to_cell_count_ - это номер самой нижней строки, в которой хранится ячейка, для которой вызван SetCell
+        // последний элемент в column_to_cell_count_ - это номер самой последней колонки, в которой хранится ячейка, для которой вызван SetCell
+        return {
+            row_to_cell_count_.rbegin()->first + 1,
+            column_to_cell_count_.rbegin()->first + 1
+        };
+    }
+
+    void PrintValues(std::ostream& output) const override {
+        const std::function<void(const CellInterface&)> print_cell = [&output](const CellInterface& cell){
+            std::visit([&output](const auto &elem) { output << elem; }, cell.GetValue());
+        };
+        PrintCells(output, print_cell);
+    }
+    void PrintTexts(std::ostream& output) const override {
+        std::function<void(const CellInterface&)> print_cell = [&output](const CellInterface& cell){
+            output << cell.GetText();
+        };
+        PrintCells(output, print_cell);
+    }
+
+    // Методы получения ячейки, независимо от того, был ли вызван SetCell в этой ячейке
+    const Cell* GetConcreteCell(Position pos) const {
+        if (!HasConcreteCell(pos)) {
+            return nullptr;
+        }
+        return cells_.at(pos).get();
+    }
+    Cell* GetConcreteCell(Position pos) {
+        if (!HasConcreteCell(pos)) {
+            return nullptr;
+        }
+        return cells_[pos].get();
+    }
 
 private:
-    void MaybeIncreaseSizeToIncludePosition(Position pos);
-    void PrintCells(std::ostream& output,
-                    const std::function<void(const CellInterface&)>& printCell) const;
-    Size GetActualSize() const;
+    bool HasCell(Position pos) const {
+        auto cell = GetConcreteCell(pos);
+        return cell && !cell->IsEmpty();
+    }
 
-    std::vector<std::vector<std::unique_ptr<Cell>>> cells_;
+    bool HasConcreteCell(Position pos) const {
+        if (!pos.IsValid()) {
+            throw InvalidPositionException("cell check error: position is invalid"s);
+        }
+        return cells_.count(pos);
+    }
+
+    void PrintCells(std::ostream& output, const std::function<void(const CellInterface&)>& printCell) const {
+        auto size = GetPrintableSize();
+        if (size == Size{}) {
+            return;
+        }
+
+        for (int i = 0; i < size.rows; ++i) {
+            for (int j = 0; j < size.cols; ++j) {
+                if (j != 0) {
+                    output << '\t';
+                }
+                Position pos{i, j};
+                if (!HasConcreteCell(pos)) {
+                    output << ""s;
+                } else {
+                    printCell(*cells_.at(pos));
+                }
+            }
+            output << '\n';
+        }
+    }
+
+    // Ячейки
+    std::unordered_map<Position, std::unique_ptr<Cell>, PositionHasher> cells_;
+    // Количество элементов в строке: номер строки - количество ячеек, которые у которых выполнен SetCell
+    std::map<int, int> row_to_cell_count_;
+    // Количество элементов в столбце: номер столбца - количество ячеек, которые у которых выполнен SetCell
+    std::map<int, int> column_to_cell_count_;
 };
